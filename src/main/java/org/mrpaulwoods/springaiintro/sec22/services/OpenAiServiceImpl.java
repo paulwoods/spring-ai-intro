1ab235cf728b07f69c179f32bfab76563ec3c982
mrpaulwoods <mr.paul.woods@gmail.com>
2025-08-04 12:44:04 -0500

    feat: reverting sec 22 OpenAiServiceImpl

--- src/main/java/org/mrpaulwoods/springaiintro/sec22/services/OpenAiServiceImpl.java
+++ src/main/java/org/mrpaulwoods/springaiintro/sec22/services/OpenAiServiceImpl.java
@@ -1,8 +1,5 @@
 package org.mrpaulwoods.springaiintro.sec22.services;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.JsonNode;
-import com.fasterxml.jackson.databind.ObjectMapper;
 import org.mrpaulwoods.springaiintro.sec22.model.Answer;
 import org.mrpaulwoods.springaiintro.sec22.model.GetCapitalRequest;
 import org.mrpaulwoods.springaiintro.sec22.model.Question;
@@ -10,7 +7,6 @@ import org.springframework.ai.chat.model.ChatModel;
 import org.springframework.ai.chat.model.ChatResponse;
 import org.springframework.ai.chat.prompt.Prompt;
 import org.springframework.ai.chat.prompt.PromptTemplate;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.io.Resource;
 import org.springframework.stereotype.Service;
@@ -43,25 +39,12 @@ public class OpenAiServiceImpl implements OpenAiService {
         return answer;
     }
 
-    @Autowired
-    ObjectMapper objectMapper;
-
     @Override
     public Answer getCapital(GetCapitalRequest getCapitalRequest) {
         PromptTemplate promptTemplate = new PromptTemplate(getCapitalPrompt);
         Prompt prompt = promptTemplate.create(Map.of("stateOrCountry", getCapitalRequest.stateOrCountry()));
         ChatResponse response = chatModel.call(prompt);
-        System.out.println(response.getResult().getOutput().getText());
-
-        String responseString;
-        try {
-            JsonNode jsonNode = objectMapper.readTree(response.getResult().getOutput().getText());
-            responseString = jsonNode.get("answer").asText();
-        } catch (JsonProcessingException e) {
-            throw new RuntimeException(e);
-        }
-
-        return new Answer(responseString);
+        return new Answer(response.getResult().getOutput().getText());
     }
 
     @Override
@@ -79,6 +62,5 @@ public class OpenAiServiceImpl implements OpenAiService {
         ChatResponse response = chatModel.call(prompt);
         return response.getResult().getOutput().getText();
     }
-
 
 }
